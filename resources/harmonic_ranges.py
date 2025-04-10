41d9d227c82a389bcdec3319804113e7a42b2b13
gerard <12766134+scripting-drafts@users.noreply.github.com>
2025-04-10 15:40:02 +0200

    frequency precision update

--- resources/harmonic_ranges.py
+++ resources/harmonic_ranges.py
@@ -1,4 +1,16 @@
 def get_harmonic_range(hr='medium'):
+        '''
+        SUB
+        9 - A - 13.57hz
+        33- A1- 55
+
+        BASS
+        33- A1- 55
+        45- A2- 110hz
+
+
+        93- A6- 1760
+        '''
         if hr == 'higher':
             n = [x for x in range(69, 94)]
             return n
@@ -13,6 +25,12 @@ def get_harmonic_range(hr='medium'):
 
         elif hr == 'medium':
             n = [x for x in range(33, 94)]
+
+        elif hr == 'bass':  # 16 -> E 20.60hz
+            n = [x for x in range(33, 46)]
+
+        elif hr == 'sub':  # 16 -> E 20.60hz
+            n = [x for x in range(9, 34)]
 
         else:
             exit()
